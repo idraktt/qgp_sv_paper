48f7aa94e13d0a18e441cec544e609886d9d5467
idraktt <idraktt@gmail.com>
2025-11-13 14:49:53 -0700

    Update 3_filter_quality.sh

--- code/3_filter_quality.sh
+++ code/3_filter_quality.sh
@@ -7,25 +7,25 @@ filter_calls_quality() {
         name=$(echo $fname | cut -f 1 -d '.');
         IFS='- ' read -r -a array <<< "$name";
         family="${array[0]}";
-        echo -e "bcftools view -i '(SVTYPE = \"DEL\" & (INFO/num_Reads[0] > 10) )' $breakdancer_path/lowmq/$name.lowmq.vcf > $breakdancer_path/filtered/$name.filtered.vcf" | bsub -n 1 -e $reports_path/logs/$name-err.txt -o $reports_path/logs/$name-out.txt -q qgp -P ${projectname};
+        echo -e "bcftools view -i '(SVTYPE = \"DEL\" & (INFO/num_Reads[0] > 2) )' $breakdancer_path/lowmq/$name.lowmq.vcf > $breakdancer_path/filtered/$name.filtered.vcf" | bsub -n 1 -e $reports_path/logs/$name-err.txt -o $reports_path/logs/$name-out.txt -q qgp -P ${projectname};
 
         echo -e "bcftools view -i '(SVTYPE = \"DEL\" & (FORMAT/PE[0] > 0) )' $breakseq_path/lowmq/$name.lowmq.vcf > $breakseq_path/filtered/$name.filtered.vcf" | bsub -n 1 -e $reports_path/logs/$name-err.txt -o $reports_path/logs/$name-out.txt -q qgp -P ${projectname};
 
         echo -e "bcftools view -i 'INFO/natorQ0[0] < 0.5 & INFO/natorP1[0] < 0.01' $cnvnator_path/lowmq/$name.lowmq.vcf > $cnvnator_path/filtered/$name.filtered.vcf" | bsub -n 1 -e $reports_path/logs/$name-err.txt -o $reports_path/logs/$name-out.txt -q qgp -P ${projectname};
 
-        echo -e "bcftools view -i '(SVTYPE = \"DEL\" & (INFO/PE[0] > 12 | INFO/SR[0] > 12) ) | (SVTYPE = \"DUP\" & (INFO/PE[0] > 4 | INFO/SR[0] > 4)) | (SVTYPE = \"INV\"& (INFO/PE[0] > 4 | INFO/SR[0] > 4)) ' $delly_path/lowmq/$name.lowmq.vcf > $delly_path/filtered/$name.filtered.vcf" | bsub -n 1 -e $reports_path/logs/$name-err.txt -o $reports_path/logs/$name-out.txt -q qgp -P ${projectname};
+        echo -e "bcftools view -i '(SVTYPE = \"DEL\" & (INFO/PE[0] > 4 | INFO/SR[0] > 4) ) | (SVTYPE = \"DUP\" & (INFO/PE[0] > 4 | INFO/SR[0] > 4)) | (SVTYPE = \"INV\"& (INFO/PE[0] > 4 | INFO/SR[0] > 4)) ' $delly_path/lowmq/$name.lowmq.vcf > $delly_path/filtered/$name.filtered.vcf" | bsub -n 1 -e $reports_path/logs/$name-err.txt -o $reports_path/logs/$name-out.txt -q qgp -P ${projectname};
 
         echo -e "bcftools view $erds_path/lowmq/$name.lowmq.vcf > $erds_path/filtered/$name.filtered.vcf" | bsub -n 1 -e $reports_path/logs/$name-err.txt -o $reports_path/logs/$name-out.txt -q qgp -P ${projectname};
 
         echo -e "bcftools view  $genomestrip_path/lowmq/$name.lowmq.vcf > $genomestrip_path/filtered/$name.filtered.vcf" | bsub -n 1 -e $reports_path/logs/$name-err.txt -o $reports_path/logs/$name-out.txt -q qgp -P ${projectname};
 
-        echo -e "bcftools view -i '(SVTYPE = \"DEL\" & (FORMAT/PR[0:1] > 16 | FORMAT/SR[0:1] > 16) ) | (SVTYPE = \"DUP\" & (FORMAT/PR[0:1] > 16 | FORMAT/SR[0:1] > 16)) | (SVTYPE = \"INV\" & (FORMAT/PR[0:1] > 12 | FORMAT/SR[0:1] > 12))' $manta_path/lowmq/$name.lowmq.vcf > $manta_path/filtered/$name.filtered.vcf" | bsub -n 1 -e $reports_path/logs/$name-err.txt -o $reports_path/logs/$name-out.txt -q qgp -P ${projectname};
+        echo -e "bcftools view -i '(SVTYPE = \"DEL\" & (FORMAT/PR[0:1] > 10 | FORMAT/SR[0:1] > 10) ) | (SVTYPE = \"DUP\" & (FORMAT/PR[0:1] > 16 | FORMAT/SR[0:1] > 16)) | (SVTYPE = \"INV\" & (FORMAT/PR[0:1] > 12 | FORMAT/SR[0:1] > 12))' $manta_path/lowmq/$name.lowmq.vcf > $manta_path/filtered/$name.filtered.vcf" | bsub -n 1 -e $reports_path/logs/$name-err.txt -o $reports_path/logs/$name-out.txt -q qgp -P ${projectname};
         
         echo -e "bcftools view -i '(SVTYPE = \"DEL\" & (INFO/PE[0] > 4 | INFO/SR[0] > 4)) | (SVTYPE = \"DUP\" & (INFO/PE[0] > 5 | INFO/SR[0] > 5)) | (SVTYPE = \"INV\"& (INFO/PE[0] > 10 | INFO/SR[0] > 10))' $speedseq_path/lowmq/$name.lowmq.vcf > $speedseq_path/filtered/$name.filtered.vcf" | bsub -n 1 -e $reports_path/logs/$name-err.txt -o $reports_path/logs/$name-out.txt -q qgp -P ${projectname};
         
-        echo -e "bcftools view -i '(SVTYPE = \"DEL\" & (FORMAT/AD[0] > 12) ) | (SVTYPE = \"DUP\" & (FORMAT/AD[0] > 12)) | (SVTYPE = \"INV\"& (FORMAT/AD[0] > 12)) ' $svaba_path/lowmq/$name.lowmq.vcf > $svaba_path/filtered/$name.filtered.vcf" | bsub -n 1 -e $reports_path/logs/$name-err.txt -o $reports_path/logs/$name-out.txt -q qgp -P ${projectname};
+        echo -e "bcftools view -i '(SVTYPE = \"DEL\" & (FORMAT/AD[0] > 16) ) | (SVTYPE = \"DUP\" & (FORMAT/AD[0] > 16)) | (SVTYPE = \"INV\"& (FORMAT/AD[0] > 14)) ' $svaba_path/lowmq/$name.lowmq.vcf > $svaba_path/filtered/$name.filtered.vcf" | bsub -n 1 -e $reports_path/logs/$name-err.txt -o $reports_path/logs/$name-out.txt -q qgp -P ${projectname};
         
-        echo -e "bcftools view -i '(SVTYPE = \"DEL\" & (INFO/A[0] > 8) ) | (SVTYPE = \"DUP\" & (INFO/A[0] > 8))| (SVTYPE = \"INV\" & (INFO/A[0] > 6 ))' $wham_path/lowmq/$name.lowmq.vcf > $wham_path/filtered/$name.filtered.vcf" | bsub -n 1 -e $reports_path/logs/$name-err.txt -o $reports_path/logs/$name-out.txt -q qgp -P ${projectname};
+        echo -e "bcftools view -i '(SVTYPE = \"DEL\" & (INFO/A[0] > 8) ) | (SVTYPE = \"DUP\" & (INFO/A[0] > 9))| (SVTYPE = \"INV\" & (INFO/A[0] > 6 ))' $wham_path/lowmq/$name.lowmq.vcf > $wham_path/filtered/$name.filtered.vcf" | bsub -n 1 -e $reports_path/logs/$name-err.txt -o $reports_path/logs/$name-out.txt -q qgp -P ${projectname};
 
         done < "$1"     	
 }
